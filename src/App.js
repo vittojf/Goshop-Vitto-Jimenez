2cb2bbc6f92ffe01fbb0e125b76c5d97672a33c2
vittojf <vectorsjf@gmail.com>
2021-03-27 15:49:28 -0400

    V0.1

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p style={{fontSize:'30pt'}}>
+         GoShop
+        </p>
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
